18d8f33cf8f6f022dd3ce65798195b0de0382a9b
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-08 13:41:27 +0300

    Initial Commit

---
+++ SwiftUIWaterWaveSlider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIWaterWaveSlider
+//
+//  Created by Ramill Ibragimov on 08.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIWaterWaveSlider/SwiftUIWaterWaveSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWaterWaveSliderApp.swift
+//  SwiftUIWaterWaveSlider
+//
+//  Created by Ramill Ibragimov on 08.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWaterWaveSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
